99e5258cbd75bdca96be64c99d0f54a2593bdd4e
Mateus Rocha Battú <2020312976@FARROUPILHA.IFRS.EDU.BR>
2023-03-17 15:23:20 -0300

    gráfico e legendas

--- geragrafico.py
+++ geragrafico.py
@@ -1,10 +1,17 @@
 from leitorarquivo import LeitorArquivo
 
+import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
     leitor = LeitorArquivo('dado.txt')
     valores = leitor.getValores()
     print(valores)
 
+
+    plt.plot(valores)
+    plt.ylabel('Valores de entrada')
+    plt.xlabel('Amostragem')
+    plt.show()
 
 main()
